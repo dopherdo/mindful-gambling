2cd2d27006dc755b3bf032b8ae0f20dd101bcf5a
Chris Yeh <dopherdo@users.noreply.github.com>
2026-03-06 00:05:21 -0800

    Fixed secrets and added logo file

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || "AIzaSyCAnzmw0KzQ-FqePHxTjoPibiKPd4gRDIE",
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || "mindful-gambling-c1504.firebaseapp.com",
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || "mindful-gambling-c1504",
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || "mindful-gambling-c1504.firebasestorage.app",
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || "292004695524",
-  appId: process.env.REACT_APP_FIREBASE_APP_ID || "1:292004695524:web:73ee8159d3d4d7fcb68bf1",
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID || "G-YF4HYB1PDN",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
